1b3c47fb8ee2aa5a4e7d227c2954202b4cb4409a
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-01-14 01:29:20 +0530

    simplify example

--- index.ts
+++ index.ts
@@ -1,49 +1,11 @@
 import fastify from "fastify";
-import { db } from "./db";
+
 const server = fastify({
   logger: true,
 });
 
 server.get("/", async (request, reply) => {
-  return { healthy: true };
-});
-
-server.get("/users", async (request, reply) => {
-  const users = await db.user.findMany();
-  reply.send({ users });
-});
-server.get<{
-  Params: {
-    id: string;
-  };
-}>("/users/:id", async (request, reply) => {
-  const { id } = request.params;
-  const user = await db.user.findFirst({
-    where: {
-      id,
-    },
-  });
-  if (!user) {
-    reply.send({ error: "user not found" });
-  } else {
-    reply.send({ user });
-  }
-});
-
-server.post<{
-  Body: {
-    name: string;
-    email: string;
-  };
-}>("/users", async (request, reply) => {
-  const { name, email } = request.body;
-  const user = await db.user.create({
-    data: {
-      name,
-      email,
-    },
-  });
-  reply.send({ user });
+  return { healthy: true, version: "1.0.0" };
 });
 
 server.listen({
