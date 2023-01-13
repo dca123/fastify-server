10431f5fe7d3fb26d43b764d50785f1fd70c96f6
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-01-14 01:33:40 +0530

    dotenv

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import fastify from "fastify";
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const server = fastify({
   logger: true,
 });
 
 server.get("/", async (request, reply) => {
-  return { healthy: true, version: "1.0.0" };
+  return { healthy: true, version: "1.0.0", secret: process.env.SECRET };
 });
 
 server.listen({
